7aaf2986034fe18ab323e1fe1274357b8113249b
JustinRM104 <55231950+JustinRM104@users.noreply.github.com>
2020-03-03 14:57:10 +0100

    Update lab04.js

--- week2/lab04/lab04.js
+++ week2/lab04/lab04.js
@@ -2,16 +2,16 @@
 let palet = new Array('wit','blauw','groen','rood','zwart');
 document.write('<br />palet is nu: ' + palet);
 
-let nieuwekleur = prompt('paars');
+let nieuwekleur = prompt('Nieuwe kleur');
 
 // Stop de nieuwekleur ACHTERAAN in de palet array
 palet.push(nieuwekleur);
 document.write('<br />palet is nu: ' + palet);
 
 // Stop de nieuwekleur VOORAAN in de palet array
-let nieuwekleur = prompt( 'Typ een nieuwe kleur in:');
-palet.unshift(nieuwekleur);
+let nieuwekleur2 = prompt('Typ een nieuwe kleur in:');
+palet.unshift(nieuwekleur2);
 document.write('<br />palet is nu: ' + palet);
 
-plaet.sort();
+palet.sort(); // Op alphabetische volgorde
 document.write('<br />palet is nu: ' + palet);
