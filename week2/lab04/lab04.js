d413b02747ab6b885bb47a6a7f97cf6dfa4b4eec
JustinRM104 <55231950+JustinRM104@users.noreply.github.com>
2020-03-03 13:55:56 +0100

    verbeterd

--- week2/lab04/lab04.js
+++ week2/lab04/lab04.js
@@ -1,16 +1,17 @@
 // Pas het script aan volgens de richtlijnen
-var palet = new Array('wit','blauw','groen','rood','zwart');
+let palet = new Array('wit','blauw','groen','rood','zwart');
 document.write('<br />palet is nu: ' + palet);
 
-var nieuwekleur = prompt('Typ een nieuwe kleur in');
+let nieuwekleur = prompt('paars');
 
 // Stop de nieuwekleur ACHTERAAN in de palet array
+palet.push(nieuwekleur);
 document.write('<br />palet is nu: ' + palet);
 
 // Stop de nieuwekleur VOORAAN in de palet array
-var nieuwekleur = prompt( 'Typ een nieuwe kleur in:');
-
+let nieuwekleur = prompt( 'Typ een nieuwe kleur in:');
+palet.unshift(nieuwekleur);
 document.write('<br />palet is nu: ' + palet);
-// Sorteer de array
 
+plaet.sort();
 document.write('<br />palet is nu: ' + palet);
